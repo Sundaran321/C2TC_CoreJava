8977b3714cf3d46162328fbdfd4c44fe27487a5d
ADMIN <ADMIN@DESKTOP-J4DU5A7>
2024-04-17 06:31:51 +0530

    HelloWorld

--- src/com/sundaran/ass2/commision.java
+++ src/com/sundaran/ass2/commision.java
@@ -15,29 +15,33 @@ public class commision
   	this.salesAmount=salesAmount;
    }
 
-   
-  public double CalculateCommission(){ 
-  	 if (salesAmount>=100000){
-  		 return commision  = salesAmount*0.1;
-  	 }
-  	 else if(50000<=salesAmount && salesAmount<100000) {
-  		 return  commision = salesAmount*0.05;
-  	 }
-  	 else if(30000<=salesAmount && salesAmount<50000) {
-  		 return  commision=salesAmount*0.03;
-  	 }
-  	 else  {
-  		 return  0;
-  	 }
-  }
 
-  public static void main(String[] args) {
-Person student=new Person();
-  	commision commission =new commision();
-  	commission.AcceptDetails_Employee("Riswana","1057 vanji nagar",996754890,90000);
-  	double commisionAmount =commission.CalculateCommission();
-  	System.out.println("Commission amount:$"+commisionAmount);
-  	
+	public double CalculateCommission() {
+		if(salesAmount >= 100000)
+			return salesAmount*0.01;
+		else if(50000 <= salesAmount && salesAmount < 100000 )
+			return salesAmount*0.05;
+		else if(30000 <= salesAmount && salesAmount < 50000 )
+			return salesAmount*0.03;
+		else
+			return 0;
+		
 
   	}
-}
+
+
+
+		public static void main(String[] args) {
+		Person student =new Person();
+		commision commission =new commision();
+		commission.AcceptDetails_Employee("siva","26 north stree",93448215,80000);
+		double commisionAmount =commission.CalculateCommission();
+		System.out.println("Commission amount:$"+commisionAmount);
+		
+
+		}
+
+	}
+
+
+
